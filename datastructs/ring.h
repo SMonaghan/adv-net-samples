4df8b3f568bb4cee119c82a189e0581da3267596
SMonaghan <Stephenjmonaghan@gmail.com>
2015-01-19 21:55:19 -0500

    Adding in the ring buffer

--- datastructs/ring.h
+++ datastructs/ring.h
@@ -1,28 +1,102 @@
-#ifndef RING_H
-#define RING_H
-
-/* A ring buffer for multi-threaded programs. A single producer pushes
- * objects into the ring, while a single consumer pops them off for
- * processing. If the ring is full (i.e., the consumer is slower than the
- * producer), then the push operation fails.
+/*Author Stephen Monaghan
+  Class Advanced Networking
+  Professor Tim Wood
 */
+#include <stdio.h>
+#include <stdlib.h>
+#define BUFFER_LENGTH 16
 
+struct ring* rb_create(void);
+void rb_delete(struct ring *rb);
+int rb_isempty(struct ring *rb);
+int rb_isfull(struct ring *rb);
+int rb_enqueue(struct ring *rb, void *value);
+void* rb_dequeue(struct ring *rb);
+
+/*
+* This is the ring struct
+* it holds an array to store the ring buffer
+* also the head which points to the first element
+* and the tail which points to the last element
+ */
 struct ring{
-        /* Fill this in */
+	void* rb[BUFFER_LENGTH];
+	int head;
+	int tail;
+};
+
+/*
+* Initializes the ring buffer
+ */
+struct ring* rb_create(void){
+	struct ring* rb = malloc(sizeof(struct ring));
+	rb->head = 0;
+	rb->tail = 0;
 }
 
-/* Create a ring buffer with the specified size. Return the ring or NULL
-   if there is not enough memory to create. */
-struct ring* ring_create(int size);
+/*
+* adds and element to the array at the tail
+ */
+int rb_enqueue(struct ring *rb, void *value){
+	if(rb_isfull(rb)){
+		return NULL;
+	}
+	rb->rb[(rb->tail)] = value;
+	rb->tail = (rb->tail + 1) % (BUFFER_LENGTH);
+	return -1;
+}
 
-/* Add an entry to the ring.
-        Return 0 on success, or a sensible error code if ring is full
-*/
-int ring_push(struct ring *rb, void* data);
+/*
+* removes the element at the head
+ */
+void* rb_dequeue(struct ring *rb){
+	if(rb_isempty(rb)){
+		return NULL;
+	}
+	void *val = rb->rb[rb->head];
+	rb->rb[rb->head] = NULL;
+	rb->head = (rb->head+1) % BUFFER_LENGTH;
+	return val;
+}
 
-/* Remove an entry from the ring.
-        Return a pointer to the data, or NULL if empty
-*/
-void* ring_pop(struct ring *rb);
+/*
+* checks the ring buffer to see if it if full
+ */
+int rb_isfull(struct ring *rb){
+	return (rb->tail + 1) % BUFFER_LENGTH == rb->head;
+}
 
-#endif
+/*
+* checks to see if the ring buffer is empty
+ */
+int rb_isempty(struct ring *rb){
+	if(rb->tail == rb->head){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+/*
+* clears the ring buffer
+ */
+void rb_delete(struct ring *rb){
+	while(rb_dequeue(rb) != NULL){
+		rb_dequeue(rb);
+	}
+	free(rb);
+}
+
+/*
+* prints the ring buffer
+ */
+void printer(struct ring *rb){
+	int pos = rb->head;
+	do{
+		printf("%d\n",rb->rb[pos]);
+		pos++;
+		if (pos == BUFFER_LENGTH){
+			pos = 0;
+		}
+	}while(pos != rb->tail);
+}
